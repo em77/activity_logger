32547306d26f8101f1d3f03839fbf1b4d7c0b0d7
Eric M <ericmarchese77@gmail.com>
2016-02-21 22:33:28 -0500

    Added user show page spec

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,1 +1,11 @@
+require "rails_helper"
 
+feature "User show page", :sorcery do
+
+  scenario "user's email is displayed" do
+    user = create(:user, :admin)
+    signin(user.email, "password")
+    visit user_path(user)
+    expect(page).to have_content user.email
+  end
+end
