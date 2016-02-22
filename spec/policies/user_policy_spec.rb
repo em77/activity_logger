d4be88db477651316176152fb0192c556ee41da7
Eric M <ericmarchese77@gmail.com>
2016-02-21 22:12:03 -0500

    Added show policy spec, passing

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -9,11 +9,25 @@ describe UserPolicy do
 
   permissions :index? do
     it "denies access if not admin" do
-      expect(UserPolicy).not_to permit(current_user)
+      expect(subject).not_to permit(current_user)
     end
 
     it "gives access for admin" do
-      expect(UserPolicy).to permit(admin)
+      expect(subject).to permit(admin)
+    end
+  end
+
+  permissions :show? do
+    it "prevents second_user from seeing show page" do
+      expect(subject).not_to permit(current_user, second_user)
+    end
+
+    it "allows user to see own show page" do
+      expect(subject).to permit(current_user, current_user)
+    end
+
+    it "allows admin to see show page of other users" do
+      expect(subject).to permit(admin)
     end
   end
 end
