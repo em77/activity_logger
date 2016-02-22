46652f1a9559e422d3194ed152fd509f757968f8
Eric M <ericmarchese77@gmail.com>
2016-02-21 22:06:24 -0500

    Added index policy spec, passing

---
+++ spec/policies/user_policy_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe UserPolicy do
+  subject {UserPolicy}
+
+  let(:current_user) {FactoryGirl.build_stubbed :user}
+  let(:second_user) {FactoryGirl.build_stubbed :user}
+  let(:admin) {FactoryGirl.build_stubbed :user, :admin}
+
+  permissions :index? do
+    it "denies access if not admin" do
+      expect(UserPolicy).not_to permit(current_user)
+    end
+
+    it "gives access for admin" do
+      expect(UserPolicy).to permit(admin)
+    end
+  end
+end
