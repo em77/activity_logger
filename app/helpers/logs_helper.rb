432a45bc17feb0ec726f78244f5bf381f17de8c7
Eric M <ericmarchese77@gmail.com>
2016-02-29 17:00:31 -0500

    Added status toggle button to Log show page, moved css logic to helper method

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -2,4 +2,12 @@ module LogsHelper
   def status_id_maker(id)
     "status-" + id.to_s
   end
+
+  def status_css_maker(log)
+    if log.processed?
+      "glyphicon glyphicon-ok status_processed"
+    else
+      "status_not_processed"
+    end
+  end
 end
