fd55d4d3d5581dc91e757e9df275069215896fcc
Eric M <ericmarchese77@gmail.com>
2016-03-07 22:31:02 -0500

    Added www to custom domain in previous changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mailer settings
-  config.action_mailer.default_url_options = { host: "#{ENV["DOMAIN"]}.com" }
+  config.action_mailer.default_url_options = { host: "www.#{ENV["DOMAIN"]}.com" }
 end
